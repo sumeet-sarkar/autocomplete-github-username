40d5309bce8d9f0358526389bc304981badaa4de
Sumeet <sumeet.somu@gmail.com>
2022-06-13 13:25:09 +0530

    fetching results from github api

---
+++ src/api/getUsers.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+interface IUserItem {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  received_events_url: string;
+  type: string;
+  score: number;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  events_url: string;
+  site_admin: boolean;
+}
+
+export interface IUser {
+  total_count: number;
+  incomplete_results: false;
+  items: IUserItem[];
+}
+
+export async function getUsers(query: string) {
+  const response = await axios.get("https://api.github.com/search/users", {
+    headers: {
+      Accept: "application/vnd.github.v3+json",
+    },
+    params: {
+      q: query,
+    },
+  });
+
+  return response;
+}
--- src/components/search-bar/SearchBar.tsx
+++ src/components/search-bar/SearchBar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { getUsers, IUser } from "../../api/getUsers";
 import searchIcon from "../../assets/search.svg";
 import SearchResult from "../search-result/SearchResult";
 
@@ -6,12 +7,28 @@ import "./SearchBar.css";
 
 function SearchBar() {
   const [input, setInput] = React.useState("");
-  
+  const [results, setResults] = React.useState<IUser | null>(null);
+
+  const fetchResults = async (input: string) => {
+    if (input === "") {
+      setResults(null);
+      return;
+    }
+    try {
+      const res = await getUsers(input);
+      const newResults = { ...(res.data as IUser) };
+      newResults.items = newResults.items.slice(0, 5);
+      setResults(newResults);
+    } catch (err) {
+      console.error("can not fetch users. ", err);
+    }
+  };
+
   function onChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
     setInput(e.target.value);
+    fetchResults(e.target.value);
   }
 
-  console.log(input)
   return (
     <div className="searchbar-container">
       <div className="search-bar">
